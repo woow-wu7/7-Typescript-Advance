047d0e189c332328b107ea6ba599a428fdea693c
woow_wu7 <wuxia@shouqianba.com>
2021-08-21 17:02:32 +0800

    feat: tsconfig.json2

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
+// 1
 // tsc src/*.ts 编译所有scr文件夹中的ts文件
+
+// 2
+// 测试 tsconfig.json中的 target 和 module，即 ( 编译后的js的版本类型和模块类型 )
 
 import { other } from './other'
 import { reverse } from '@/util/index'
